73b2f7ff357ef4c0aa72e0c60850d4aa5ff645fa
wilsonmesquita03 <wilsonbrasilplauforever@gmail.com>
2024-08-08 11:28:42 -0300

    fix: normalizeUrl

--- src/utils/normalize.ts
+++ src/utils/normalize.ts
@@ -1,26 +1,37 @@
 export const isExternalUrl = (url: string): boolean => {
-  // Expressão regular para verificar se a URL começa com http ou https
-  const externalUrlPattern = /^(http|https):\/\//;
-  return externalUrlPattern.test(url);
-}
+  try {
+    const { hostname } = new URL(url);
+    // Verifica se o hostname é localhost ou 127.0.0.1
+    return hostname !== "localhost" && hostname !== "127.0.0.1";
+  } catch (error) {
+    // Retorna true em caso de erro na URL (por exemplo, URL inválida)
+    return true;
+  }
+};
 
 export const normalizeFileUrl = (url: string): string => {
   if (isExternalUrl(url)) {
     return url; // URLs externas já estão normalizadas
   } else {
     // Obter a URL base da variável de ambiente
-    const baseUrl = process.env.NEXT_PUBLIC_API_URL?.replace("/api", "/uploads");
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL?.replace(
+      "/api",
+      "/uploads"
+    );
 
     if (!baseUrl) {
-      throw new Error('A variável de ambiente NEXT_PUBLIC_API_URL não está definida');
+      throw new Error(
+        "A variável de ambiente NEXT_PUBLIC_API_URL não está definida"
+      );
     }
 
     // Adicionar barra inicial se não houver uma
-    if (!url?.startsWith('/')) {
-      url = '/' + url;
+    if (!url?.startsWith("/")) {
+      url = "/" + url;
     }
 
     // Construir a URL completa
     return new URL(url, baseUrl).toString();
   }
-}
+};
+
